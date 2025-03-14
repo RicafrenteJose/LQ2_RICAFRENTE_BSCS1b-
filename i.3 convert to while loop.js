8e943d408269e219b72d4b7c6a1783d13db69fc3
RicafrenteJose <198441580+RicafrenteJose@users.noreply.github.com>
2025-03-14 15:07:35 +0800

    Update i.3 convert to while loop.js

---
+++ i.3 convert to while loop.js
@@ -0,0 +1,10 @@
+let fname = "Jose Cruz";
+let i = 0;// Initialization
+
+while(fname.length > i) {// Condition
+    let c = 2 + i;
+    let nLengthCounterTwo = fname.length - c;
+    console.log(fname);
+    console.log("Iterations Name was Printed: " + i);
+    i++; // Iteration
+}
